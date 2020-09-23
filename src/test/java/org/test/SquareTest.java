531492d9ef60db19c365017544f232d42ebdd1d6
AlfaBravo <alexandrbedya@gmail.com>
2020-09-23 15:43:46 +0300

    added assert

--- src/test/java/org/test/SquareTest.java
+++ src/test/java/org/test/SquareTest.java
@@ -1,5 +1,6 @@
 package org.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTest {
@@ -7,7 +8,7 @@ public class SquareTest {
     @Test
     public void testArea() {
         Square s = new Square(5);
-        assert s.area() == 25;
+        Assert.assertEquals(s.area(), 25.0);
     }
 
 }
