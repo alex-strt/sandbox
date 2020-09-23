b38ff7693d9cee7bea54793b20eef616541d734e
AlfaBravo <alexandrbedya@gmail.com>
2020-09-23 15:08:20 +0300

    added Test Class

---
+++ src/test/java/org/test/SquareTest.java
@@ -0,0 +1,13 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class SquareTest {
+
+    @Test
+    public void testArea() {
+        Square s = new Square(5);
+        assert s.area() == 25;
+    }
+
+}
